ed43d663138eaecd8d5d09737fc9b22d7fa54482
atsushi130 <atsushi_main@outlook.jp>
2018-03-06 09:17:57 +0900

    add: NibDesignable protocol

---
+++ SwiftExtensions/NibDesignable.swift
@@ -0,0 +1,26 @@
+//
+//  NibDesignable.swift
+//  SwiftExtensions
+//
+//  Created by Atsushi Miyake on 2018/01/03.
+//  Copyright © 2018年 Atsushi Miyake. All rights reserved.
+//
+
+import UIKit
+
+public protocol NibDesignable: class {}
+
+public extension NibDesignable where Self: UIView {
+    
+    private func instantiate() -> UIView {
+        let bundle = Bundle(for: type(of: self))
+        let nib    = UINib(nibName: self.className, bundle: bundle)
+        return nib.instantiate(withOwner: self).first as? UIView ?? UIView()
+    }
+    
+    public func configureNib() {
+        let view = self.instantiate()
+        self.insertSubview(view, at: 0)
+        view.fillSuperview()
+    }
+}
