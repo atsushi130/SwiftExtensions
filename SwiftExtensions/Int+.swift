8b20b52ee31d7a3e20021ba169cb824235cb0f07
atsushi130 <atsushi_main@outlook.jp>
2018-03-06 09:18:21 +0900

    add: Int extensions

---
+++ SwiftExtensions/Int+.swift
@@ -0,0 +1,16 @@
+//
+//  Int+.swift
+//  SwiftExtensions
+//
+//  Created by Atsushi Miyake on 2018/03/06.
+//  Copyright © 2017年 Atsushi Miyake. All rights reserved.
+//
+
+import Foundation
+
+public extension Int {
+    
+    public var half: Double {
+        return Double(self) / 2.0
+    }
+}
