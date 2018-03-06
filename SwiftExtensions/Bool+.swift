30f2630e1815e008a4eac45eb7dd4c600835ebb9
atsushi130 <atsushi_main@outlook.jp>
2018-03-06 09:14:41 +0900

    add: Bool extensions

---
+++ SwiftExtensions/Bool+.swift
@@ -0,0 +1,13 @@
+//
+//  Bool+.swift
+//  SwiftExtensions
+//
+//  Created by Atsushi Miyake on 2018/03/06.
+//  Copyright © 2017年 Atsushi Miyake. All rights reserved.
+//
+
+import Foundation
+
+public extension Bool {
+    public var toInt: Int { return self ? 1 : 0 }
+}
