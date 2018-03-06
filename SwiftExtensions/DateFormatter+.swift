c03c3818292e1908faba822eea2cd9494ff8503f
atsushi130 <atsushi_main@outlook.jp>
2018-03-06 09:19:57 +0900

    fix: filename

---
+++ SwiftExtensions/DateFormatter+.swift
@@ -0,0 +1,18 @@
+//
+//  DateFormatter+.swift
+//  SwiftExtensions
+//
+//  Created by Atsushi Miyake on 2018/03/06.
+//  Copyright © 2017年 Atsushi Miyake. All rights reserved.
+//
+
+import Foundation
+
+public extension DateFormatter {
+    public static func from(locale: Locale, format: String) -> DateFormatter {
+        let formatter = DateFormatter()
+        formatter.locale     = locale
+        formatter.dateFormat = format
+        return formatter
+    }
+}
