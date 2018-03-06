a4cbda5f94db57188e2f264f8a45c44d639e2097
atsushi130 <atsushi_main@outlook.jp>
2018-03-06 09:20:26 +0900

    add: Date extensions

---
+++ SwiftExtensions/Date+.swift
@@ -0,0 +1,15 @@
+//
+//  Date+.swift
+//  SwiftExtensions
+//
+//  Created by Atsushi Miyake on 2017.
+//  Copyright © 2017年 Atsushi Miyake. All rights reserved.
+//
+
+import Foundation
+
+public extension Date {
+    public func toString(by format: String = "yyyy/MM/dd HH:mm:ss", locale: Locale = Locale.current) -> String {
+        return DateFormatter.from(locale: locale, format: format).string(from: self)
+    }
+}
