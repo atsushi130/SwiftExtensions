e4f6765bc7392a70634e19963752820fe6fec497
atsushi130 <atsushi_main@outlook.jp>
2019-02-24 17:16:35 +0900

    add: new file

---
+++ SwiftExtensions/StringDecodableBox.swift
@@ -0,0 +1,9 @@
+//
+//  StringDecodableBox.swift
+//  SwiftExtensions
+//
+//  Created by Atsushi Miyake on 2019/02/24.
+//  Copyright © 2019年 Atsushi Miyake. All rights reserved.
+//
+
+import Foundation
