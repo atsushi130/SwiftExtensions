8b504b3eb4c9e7372ee00463e7361cb296826cd6
atsushi130 <atsushi_main@outlook.jp>
2019-02-24 17:17:25 +0900

    implement: StringDecodableBox and URL confirm LosslessStringConvertible

--- SwiftExtensions/StringDecodableBox.swift
+++ SwiftExtensions/StringDecodableBox.swift
@@ -7,3 +7,21 @@
 //
 
 import Foundation
+
+public struct StringDecodableBox<T: LosslessStringConvertible>: Decodable {
+    public let value: T
+    public init(from decoder: Decoder) throws {
+        let stringValue = try decoder.singleValueContainer().decode(String.self)
+        guard let value = T(stringValue) else {
+            throw DecodingError.dataCorrupted(.init(codingPath: decoder.codingPath, debugDescription: "The string cannot cast to \(T.self)."))
+        }
+        self.value = value
+    }
+}
+
+extension URL: LosslessStringConvertible {
+    public init?(_ description: String) {
+        guard let url = URL(string: description) else { return nil }
+        self = url
+    }
+}
