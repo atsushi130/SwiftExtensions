40309dd1f3808b6cfc4e15ebe17b400e82561cb7
atsushi130 <atsushi_main@outlook.jp>
2019-03-24 19:46:23 +0900

    add: snakecased and kebabcased

--- SwiftExtensions/String+.swift
+++ SwiftExtensions/String+.swift
@@ -9,11 +9,19 @@
 import Foundation
 
 public extension String {
-    
+
     public var attributed: NSMutableAttributedString {
         return NSMutableAttributedString(string: self)
     }
-    
+
+    public func kebabcased() -> String {
+        return self.replacingOccurrences(of: "([A-Z])", with: "-$1", options: .regularExpression).lowercased()
+    }
+
+    public func snakecased() -> String {
+        return self.replacingOccurrences(of: "([A-Z])", with: "_$1", options: .regularExpression).lowercased()
+    }
+
     public func toDate(by format: String = "yyyy/MM/dd HH:mm:ss", locale: Locale = Locale.current) -> Date? {
         return DateFormatter.from(locale: locale, format: format).date(from: self)
     }
